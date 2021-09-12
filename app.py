18b5f71260e5082a732ae62b3976c1e71de48fcc
greggo58 <greggo58@gmail.com>
2021-09-12 19:06:32 +0000

    add login

--- app.py
+++ app.py
@@ -49,6 +49,32 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username already exists in database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash(f"Welcome, {request.form.get('username')}!")
+            else:
+                # invalid password match
+                flash("Incorrect username and / or password!")
+                return redirect(url_for("login"))
+
+        else:
+            # username does not exist
+            flash("Incorrect username and / or password!")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 print(os.environ.get("IP"), int(os.environ.get("PORT")))
 
 if __name__ == "__main__":
